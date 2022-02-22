ea8c9a3d305f5f3d2fec16408535f9bdb7a555f5
loublick <loublick@microsoft.com>
2022-02-21 20:38:29 -0500

    Initial revision.

---
+++ WebApplicationDemo/WebApplicationDemo/Data/MyDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationDemo.Data
+{
+    public class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationDemo.Models.Todo> Todo { get; set; }
+    }
+}
